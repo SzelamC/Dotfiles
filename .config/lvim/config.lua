ebbfab0c3d98e0cc4cf818163296c7cafd096eae
Szelam <chanszelam7725@gmail.com>
2022-04-26 00:42:11 +0800

    update

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,28 +1,22 @@
 --[[
 lvim is the global options object
-
 Linters should be
 filled in as strings with either
-a global executable or a path to
-an executable
+a global executable or a path to an executable
 ]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = false
-lvim.colorscheme = "kanagawa"
-
--- statusline
-lvim.builtin.lualine.sections.lualine_a = { "mode" }
-lvim.builtin.lualine.options.theme = "tokyonight"
+lvim.colorscheme = "onedark"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<leader>P"] = ":MarkdownPreviewToggle<cr>"
-lvim.keys.insert_mode[";;"] = "<esc>A;"
+lvim.keys.normal_mode["<leader>P"] = ":w<cr>:Glow<cr>"
+lvim.keys.insert_mode[";;"] = "<Esc>A;"
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = false
 -- edit a default keymapping
@@ -52,19 +46,32 @@ lvim.keys.insert_mode[";;"] = "<esc>A;"
 --   name = "+Trouble",
 --   r = { "<cmd>Trouble lsp_references<cr>", "References" },
 --   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
 --   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
 --   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 -- }
 
--- TODO: User Config for predefined plugins
+-- User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = false
+lvim.builtin.alpha.active = false
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.show_icons.git = 1
+
+local components = require("lvim.core.lualine.components")
+-- lvim.builtin.lualine.options.component_separators = { left = '', right = ''}
+lvim.builtin.lualine.options.section_separators = { left = "" }
+lvim.builtin.lualine.sections.lualine_a = { "mode" }
+lvim.builtin.lualine.sections.lualine_y = {
+	components.location,
+}
+lvim.builtin.lualine.options.theme = "horizon"
+
+lvim.builtin.treesitter.autotag.enable = true
+lvim.builtin.treesitter.rainbow.enable = true
+lvim.builtin.treesitter.rainbow.colors = { "#ffc649", "#ffb2a8", "##ffa6dd" }
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -75,6 +82,7 @@ lvim.builtin.treesitter.ensure_installed = {
 	"lua",
 	"python",
 	"typescript",
+	"tsx",
 	"css",
 	"rust",
 	"java",
@@ -89,16 +97,17 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- ---@usage disable automatic installation of servers
 -- lvim.lsp.automatic_servers_installation = false
 
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "" })
--- lvim.lsp.override = vim.tbl_filter(function(name)
--- 	return name ~= "ccls"
--- end, lvim.lsp.override)
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
+-- vim.tbl_map(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
@@ -113,7 +122,8 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-	{ command = "black", filetypes = { "python" } },
+	--   { command = "black", filetypes = { "python" } },
+	--   { command = "isort", filetypes = { "python" } },
 	{
 		-- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
 		command = "prettier",
@@ -121,69 +131,163 @@ formatters.setup({
 		-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
 		extra_args = { "--print-with", "100" },
 		---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "css", "scss", "html" },
+		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
 	},
 	{
 		command = "stylua",
 		filetypes = { "lua" },
 	},
 })
-
 -- -- set additional linters
-local linters = require("lvim.lsp.null-ls.linters")
-linters.setup({
-	-- { command = "flake8", filetypes = { "python" } },
-	-- {
-	-- 	-- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-	-- 	command = "shellcheck",
-	-- 	---@usage arguments to pass to the formatter
-	-- 	-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-	-- 	extra_args = { "--severity", "warning" },
-	-- 	filetypes = { "sh" },
-	-- },
-	-- {
-	-- 	command = "codespell",
-	-- 	---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-	-- 	filetypes = { "javascript", "python" },
-	-- },
-})
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--severity", "warning" },
+--   },
+--   {
+--     command = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
+-- }
 
 -- Additional Plugins
 lvim.plugins = {
-	{ "folke/tokyonight.nvim" },
 	{
 		"folke/trouble.nvim",
 		cmd = "TroubleToggle",
 	},
 	{
-		"rebelot/kanagawa.nvim",
+		"ful1e5/onedark.nvim",
+		config = require("onedark").setup({
+			comment_style = "none",
+			keyword_style = "italic",
+			function_style = "none",
+			transparent = false,
+			dev = true,
+			transparent_sidebar = true,
+			overrides = function(c)
+				--         local c = {
+				--   none = 'NONE',
+				--   bg0 = '#282c34',
+				--   bg1 = '#21252b',
+				--   bg_highlight = '#2c313a',
+				--   bg_visual = '#393f4a',
+				--   black0 = '#20232A',
+				--   blue0 = '#61afef',
+				--   blue1 = '#528bff',
+				--   cyan0 = '#56b6c2',
+				--   fg0 = '#abb2bf',
+				--   fg_dark = '#798294',
+				--   fg_gutter = '#5c6370',
+				--   fg_light = '#adbac7',
+				--   green0 = '#98c379',
+				--   orange0 = '#e59b4e',
+				--   orange1 = '#d19a66',
+				--   purple0 = '#c678dd',
+				--   red0 = '#e06c75',
+				--   red1 = '#e86671',
+				--   red2 = '#f65866',
+				--   yellow0 = '#ebd09c',
+				--   yellow1 = '#e5c07b',
+				--   dev_icons = {
+				--     blue = '#519aba',
+				--     green0 = '#8dc149',
+				--     yellow = '#cbcb41',
+				--     orange = '#e37933',
+				--     red = '#cc3e44',
+				--     purple = '#a074c4',
+				--     pink = '#f55385',
+				--     gray = '#4d5a5e',
+				--   },
+				-- }
+				return {
+					Conditional = { fg = c.purple0, style = "italic" },
+					Exception = { fg = c.purple0, style = "italic" },
+					Repeat = { fg = c.purple0, style = "italic" },
+					TSTagAttribute = { fg = c.orange1 },
+					LspDiagnosticsVirtualTextHint = { style = "bold" },
+					javascriptTSFunction = { fg = c.blue0 },
+					javascriptTSVariable = { fg = "#E26674" },
+					typescriptTSFunction = { fg = c.blue0 },
+					typescriptTSVariable = { fg = "#E26674" },
+					String = { fg = "#70F49C" },
+					-- Normal = { bg = "#ff4a5c", fg = c.dev_icons.green0 }
+					-- javascriptTSVariable = { fg = c.yellow0 }
+				}
+			end,
+		}),
 	},
 	{
-		"catppuccin/nvim",
-		as = "catppuccin",
-	},
-	{
-		"p00f/nvim-ts-rainbow",
-	},
-	{
-		"norcalli/nvim-colorizer.lua",
+		"nvim-treesitter/playground",
+		event = "BufRead",
 	},
 	{
 		"windwp/nvim-ts-autotag",
 	},
 	{
-		"folke/todo-comments.nvim",
+		"norcalli/nvim-colorizer.lua",
 		config = function()
-			require("todo-comments").setup()
+			require("colorizer").setup({ "*" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			})
 		end,
 	},
 	{
-		"iamcco/markdown-preview.nvim",
-		run = "cd app && npm install",
-		ft = "markdown",
+		"ggandor/lightspeed.nvim",
+		event = "BufRead",
+	},
+	{
+		"karb94/neoscroll.nvim",
+		event = "WinScrolled",
 		config = function()
-			vim.g.mkdp_auto_start = 1
+			require("neoscroll").setup({
+				-- All these keys will be mapped to their corresponding default scrolling animation
+				mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+				hide_cursor = true, -- Hide cursor while scrolling
+				stop_eof = true, -- Stop at <EOF> when scrolling downwards
+				use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+				respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+				cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+				easing_function = nil, -- Default easing function
+				pre_hook = nil, -- Function to run before the scrolling animation starts
+				post_hook = nil, -- Function to run after the scrolling animation ends
+			})
 		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "BufRead",
+		config = function()
+			require("todo-comments").setup({
+				colors = {
+					hint = { "#98c379", "DiagnosticsHint" },
+				},
+			})
+		end,
+	},
+	{
+		"tpope/vim-surround",
+		keys = { "c", "d", "y" },
+	},
+	{
+		"p00f/nvim-ts-rainbow",
+	},
+	{
+		"npxbr/glow.nvim",
+		ft = { "markdown" },
+		-- run = "yay -S glow"
 	},
 }
 
@@ -191,12 +295,3 @@ lvim.plugins = {
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
-
--- plugin override config
-
--- treesitter
-lvim.builtin.treesitter.rainbow.enable = false
-lvim.builtin.treesitter.rainbow.extended_mode = true
-lvim.builtin.treesitter.rainbow.max_file_lines = nil
-lvim.builtin.treesitter.rainbow.colors = { "#FFCCFB", "#45A9D3", "#FC7662" }
-lvim.builtin.treesitter.autotag.enable = true
